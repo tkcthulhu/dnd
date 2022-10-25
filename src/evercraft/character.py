886c2e296116b24b55c365d27f02a7466c0acac9
Tyler Consalvi <tyler.consalvi@student.awesomeinc.org>
2022-10-25 20:31:27 +0000

    set basic attributes, can damage

--- src/evercraft/character.py
+++ src/evercraft/character.py
@@ -8,12 +8,46 @@
 #   [x] can you change
 #   [x] Armor Class
 #   [x] Hit points
+#   [x] can attack(method?)
+#       pass in (roll, enemy)
+#   [x] attack can change
+#   damage another character
+#   [x] can we prove damage was given
+#       IF attack is successful
+#           do damage
+#       ELSE
+#           no damage
+#   [x] can we define the damage (1 for regular, 2 for critical)
+#   abilities scores applied
+#   [] character has the 6 abilities
+#   []
 
 class Character:
     def __init__(self, name, align):
         self.name = name
         self.alignment = align
         self.armor = 10
-        self.hit = 5
+        self.hitpoints = 5        
+    def attack(self, roll, enemy):
+        enemy = enemy
+        EAC = enemy.armor
+        if roll == 20:
+            self.damage('crit', enemy)
+            return 'CRIT'
+        elif EAC < roll:
+            self.damage('hit', enemy)
+            return "HIT"
+        elif EAC > roll:
+            return "MISS"
+
+    def damage(self, hit, enemy):
+        if hit == 'crit':
+            setattr(enemy, 'hitpoints', enemy.hitpoints - 2)
+            return enemy.hitpoints
+        if hit == 'hit':
+            setattr(enemy, 'hitpoints', enemy.hitpoints - 1)
+            return enemy.hitpoints
+
+
 
 
--- src/tests/test_iteration_1.py
+++ src/tests/test_iteration_1.py
@@ -1,10 +1,10 @@
 from evercraft.character import Character
 
 def test_character_exists():
-    assert Character('Doug')
+    assert Character('Doug', 'Lawful-Evil')
 
 def test_character_name():
-    Character1 = Character('Bill')
+    Character1 = Character('Bill', 'Lawful-Evil')
     assert Character1.name == 'Bill'
 
 def test_character_alignment():
@@ -15,6 +15,40 @@ def test_armor_hit():
     Character1 = Character('Chuck', 'Lawful')
     assert Character1.armor == 10
     
+def test_character_hitpoints():
+    Character1 = Character('Cuck', 'Lawful')
+    assert Character1.hitpoints == 5
+
+def test_character_attack():
+    Character1 = Character('Cuck', 'Lawful')
+    Character2 = Character('Keith', 'Lawful-Evil')    
+    assert Character1.attack(5, Character2) == 'MISS'
+
 def test_character_hit():
     Character1 = Character('Cuck', 'Lawful')
-    assert Character1.hit == 5
+    Character2 = Character('Keith', 'Lawful-Evil')
+    assert Character1.attack(20, Character2) == 'CRIT'
+
+def test_character_hit_EAC():
+    Character1 = Character('Cuck', 'Lawful')
+    Character2 = Character('Keith', 'Lawful-Evil')
+    assert Character1.attack(4, Character2) == False
+
+def test_character_hit_EAC():
+    Character1 = Character('Cuck', 'Lawful')
+    Character2 = Character('Keith', 'Lawful-Evil')
+    assert Character1.attack(15, Character2) == "HIT"
+    assert Character2.hitpoints == 4
+
+def test_character_nat_20():
+    Character1 = Character('Skisgaar', 'Chaotic-good')
+    Character2 = Character('Keith', 'Lawful-Evil')
+    assert Character1.attack(20, Character2) == "CRIT"
+
+def test_character_damaged():
+    Character1 = Character('SpaceBun', 'Chaotic-goood')
+    Character2 = Character('Keith', 'Lawful-Evil')
+    Character1.attack(20, Character2)
+    assert Character2.hitpoints == 3
+
+    
