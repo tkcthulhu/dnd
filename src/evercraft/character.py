5d128722f04e544b897dffdcbaa2721077f7c30f
Tyler Consalvi <tyler.consalvi@student.awesomeinc.org>
2022-10-27 15:38:43 +0000

    refactor #78678943857739 completed, passes all tests

--- src/evercraft/character.py
+++ src/evercraft/character.py
@@ -9,85 +9,78 @@ class Test:
             setattr(self, key, abilities[key])
 
 def set_modifier(level):
-    if level == 1:
-        return -5
-    if level == 2 or level == 3:
-        return -4
-    if level == 4 or level == 5:
-        return -3
-    if level == 6 or level == 7:
-        return -2
-    if level == 8 or level == 9:
-        return -1
-    if level == 10 or level == 11:
-        return 0
-    if level == 12 or level == 13:
-        return 1
-    if level == 14 or level == 15:
-        return 2
-    if level == 16 or level == 17:
-        return 3
-    if level == 18 or level == 19:
-        return 4
-    if level >= 20:
-        return 5
+    return math.floor((level - 10) / 2)
 
-def damage(attacker, hit, enemy):
-    enemy.current_modifiers()
-    enemy.current_condition()
-    if hit == 'crit':
-        setattr(enemy, 'current_HP', enemy.current_HP - 2 - (attacker.str_mod * 2))
-        return enemy.current_HP
-    if hit == 'hit':
-        setattr(enemy, 'current_HP', enemy.current_HP - 1 - (attacker.str_mod))
-        return enemy.current_HP
+class Combat: 
+
+    def attack(attacker, roll, enemy):
+        attacker = attacker
+        enemy = enemy
+        EAC = enemy.armor_class
+        if roll == 20:
+            Combat.damage(attacker, 'crit', enemy)
+            attacker.xp += 10
+            attacker.player_level = math.floor((attacker.xp/1000)+1)
+            return 'CRIT'
+        elif EAC < roll + attacker.str_mod:
+            Combat.damage(attacker, 'hit', enemy)
+            attacker.xp += 10
+            attacker.player_level = math.floor((attacker.xp/1000)+1)
+            return "HIT"
+        elif EAC >= roll + attacker.str_mod:
+            return "MISS"    
+
+    def damage(attacker, hit, enemy):
+        enemy.current_condition()
+        if hit == 'crit':
+            setattr(enemy, 'current_HP', enemy.current_HP - 2 - (attacker.str_mod * 2))
+            enemy.current_condition()
+            return enemy.current_HP
+        if hit == 'hit':
+            setattr(enemy, 'current_HP', enemy.current_HP - 1 - (attacker.str_mod))
+            enemy.current_condition()
+            return enemy.current_HP
 
 class Character:
 
     first = ['Chaotic', 'Neutral', 'Lawful']
 
     second = ['Evil', 'Good', 'Neutral']
+
+    DEFAULT_ATTRIBUTES = {
+        'strength' : 10, 
+        'dexterity' : 10, 
+        'constitution' : 10, 
+        'wisdom' : 10, 
+        'intelligence' : 10, 
+        'charisma' : 10,
+        'player_level' : 1
+    }
     
-    def __init__(self, name, align, strength = 10, dexterity = 10, constitution = 10, wisdom = 10, intelligence = 10, charisma = 10):
+    def __init__(self, name, align, strength = 10, dexterity = 10, constitution = 10, wisdom = 10, intelligence = 10, charisma = 10, player_level = 1, base_hp = 5):
         self.name = name
         self.alignment = align
-        self.base_hp = 5
+        self.base_hp = base_hp
         self.armor_class = 10
         self.xp = 0
-        self.player_level = 1
+        self.player_level = player_level
         self.strength = strength        
         self.dexterity = dexterity
         self.constitution = constitution
         self.wisdom = wisdom
         self.intelligence = intelligence
         self.charisma = charisma
-
-    def current_modifiers(self):
-        self.str_mod = set_modifier(self.strength)
-        self.dex_mod = set_modifier(self.dexterity)
-        self.armor_class += self.dex_mod
-        self.const_mod = set_modifier(self.constitution)
-        self.max_HP = self.base_hp + self.const_mod
+        self.armor_class += set_modifier(dexterity)
+        self.max_HP = (base_hp + set_modifier(constitution)) * player_level
+        self.current_HP = (5 + set_modifier(constitution)) * player_level
+        self.str_mod = set_modifier(strength)
+        self.dex_mod = set_modifier(dexterity)
+        self.const_mod = set_modifier(constitution)
+        self.death = False
 
     def current_condition(self):
-        self.current_HP = self.max_HP
-    
-    def attack(self, roll, enemy):
-        self.current_modifiers()
-        enemy = enemy
-        EAC = enemy.armor_class
-        if roll == 20:
-            damage(self, 'crit', enemy)
-            self.xp += 10
-            self.ll = math.floor((self.xp/1000)+1)
-            return 'CRIT'
-        elif EAC < roll + self.str_mod:
-            # self.damage('hit', enemy)
-            self.xp += 10
-            self.player_level = math.floor((self.xp/1000)+1)
-            return "HIT"
-        elif EAC >= roll + self.str_mod:
-            return "MISS"
+        if self.current_HP <= 0:
+            self.death = True
 
     def level_check(self):
         self.max_HP = self.baseHP + (self.player_level * 5) + (self.player_level * self.const_mod)
--- src/tests/test_iteration_1.py
+++ src/tests/test_iteration_1.py
@@ -26,22 +26,22 @@ def test_character_hit_points():
 def test_character_attack():
     Character1 = Character('Cuck', 'Lawful')
     Character2 = Character('Keith', 'Lawful-Evil')    
-    assert Character1.attack(5, Character2) == 'MISS'
+    assert Combat.attack(Character1, 5, Character2) == 'MISS'
 
 def test_character_hit_EAC():
     Character1 = Character('Cuck', 'Lawful')
     Character2 = Character('Keith', 'Lawful-Evil')
-    assert Character1.attack(15, Character2) == "HIT"
+    assert Combat.attack(Character1, 15, Character2) == "HIT"
 
 def test_character_nat_20():
     Character1 = Character('Skisgaar', 'Chaotic-good')
     Character2 = Character('Keith', 'Lawful-Evil')
-    assert Character1.attack(20, Character2) == "CRIT"
+    assert Combat.attack(Character1, 20, Character2) == "CRIT"
 
 def test_character_damaged():
     Character1 = Character('SpaceBun', 'Chaotic-goood')
     Character2 = Character('Keith', 'Lawful-Evil')
-    Character1.attack(20, Character2)
+    Combat.attack(Character1, 20, Character2)
     assert Character2.current_HP == 3
 
 def test_character_stats_present():
@@ -49,31 +49,25 @@ def test_character_stats_present():
     assert Character2.strength == 10
 
 def test_character_modifier():
-    Character1 = Character('SpaceBun', 'Chaotic-goood')
-    Character1.strength = 17
+    Character1 = Character('SpaceBun', 'Chaotic-goood', strength = 17)
     Str = Character1.strength
-    Character1.current_modifiers()
     assert Str == 17
     assert Character1.str_mod == 3
 
 def test_const_modifier():
     Character1 = Character('SpaceBun', 'Chaotic-goood')
-    Character1.current_modifiers()
     assert Character1.const_mod == 0
 
 
 def test_dex_modifier():
-    Character1 = Character('SpaceBun', 'Chaotic-goood')
-    Character1.dexterity = 17
-    Character1.current_modifiers()
+    Character1 = Character('SpaceBun', 'Chaotic-goood', dexterity=17)
     assert Character1.dex_mod == 3
     assert Character1.armor_class == 13
 
 def test_str_mod():
-    Character1 = Character('SpaceBun', 'Chaotic-goood')
+    Character1 = Character('SpaceBun', 'Chaotic-goood', strength=15)
     Character2 = Character('Keith', 'Lawful-Evil')
-    Character1.strength = 15
-    Character1.attack(20, Character2)
+    Combat.attack(Character1, 20, Character2)
     assert Character2.current_HP == -1
 
 def test_character_hit():
@@ -81,29 +75,25 @@ def test_character_hit():
     # str mod +2
     Character2 = Character('Keith', 'Lawful-Evil', dexterity=7)
     # dex mod -2
-    Character1.attack(9, Character2)
+    Combat.attack(Character1, 9, Character2)
 
     assert Character2.current_HP == 2
 
 def test_str_mod_roll():
 
-    Character1 = Character('Morgan', 'Chaotic-good-ish')
-    Character1.stats(15, 15, 15, 15, 15, 15)
+    Character1 = Character('Morgan', 'Chaotic-good-ish', strength=15)
     # str mod = +2 dc=12
     Character2 = Character('Keith', 'Lawful-Evil')
-    Character2.stats(17, 10, 15, 15, 15, 15)
-    # ac mod = +5 ac=10
-    assert Character1.attack(10, Character2) == 'HIT'
+    # ac mod = +10 ac=10
+    assert Combat.attack(Character1, 10, Character2) == 'HIT'
 
 def test_str_mod_roll2():
 
-    Character1 = Character('Morgan', 'Chaotic-good-ish')
-    Character1.stats(15, 15, 15, 15, 15, 15)
+    Character1 = Character('Morgan', 'Chaotic-good-ish', dexterity=15)
     # ac mod = +2 ac=12
-    Character2 = Character('Keith', 'Lawful-Evil')
-    Character2.stats(1, 10, 15, 15, 15, 15)
+    Character2 = Character('Keith', 'Lawful-Evil', strength=1)
     # str mod = -5 dc
-    assert Character2.attack(15, Character1) == 'MISS'
+    assert Combat.attack(Character2, 15, Character1) == 'MISS'
 
 def test_character_has_xp():
     Character1 = Character('Morgan', 'Chaotic-good-ish')
@@ -111,74 +101,77 @@ def test_character_has_xp():
 
 
 def test_charcater_gain_xp():
-    Character1 = Character('Morgan', 'Chaotic-good-ish')
-    Character1.stats(15, 15, 15, 15, 15, 15)
+    Character1 = Character('Morgan', 'Chaotic-good-ish', strength=15)
     # str mod = +2 dc=12
     Character2 = Character('Keith', 'Lawful-Evil')
-    Character2.stats(17, 10, 15, 15, 15, 15)
-    # ac mod = +5 ac=10
-    Character1.attack(10, Character2)
+    # ac mod = +0 ac=10
+    Combat.attack(Character1, 10, Character2)
     assert Character1.xp == 10
 
 def test_charcater_gain_xp_two_times():
-    Character1 = Character('Morgan', 'Chaotic-good-ish')
-    Character1.stats(15, 15, 15, 15, 15, 15)
+    Character1 = Character('Morgan', 'Chaotic-good-ish', strength=15)
     # str mod = +2 dc=12
     Character2 = Character('Keith', 'Lawful-Evil')
-    Character2.stats(17, 10, 15, 15, 15, 15)
-    # ac mod = +5 ac=10
+    # ac mod = +0 ac=10
     Character1.xp = 980
 
-    Character1.attack(10, Character2)
-    Character1.attack(20, Character2)
+    Combat.attack(Character1, 10, Character2)
+    Combat.attack(Character1, 20, Character2)
 
     assert Character1.xp == 1000
 
 def test_character_has_level():
     Character1 = Character('Morgan', 'Chaotic-good-ish')
-    assert Character1.level
+    assert Character1.player_level
 
 def test_character_can_level_up():
-    Character1 = Character('Morgan', 'Chaotic-good-ish')
-    Character1.stats(15, 15, 15, 15, 15, 15)
+    Character1 = Character('Morgan', 'Chaotic-good-ish', strength=15)
 
     Character2 = Character('Keith', 'Lawful-Evil')
-    Character2.stats(17, 10, 15, 15, 15, 15)
 
     Character1.xp = 990
 
-    Character1.attack(20, Character2)
+    Combat.attack(Character1, 20, Character2)
 
     assert Character1.xp == 1000
     assert Character1.player_level == 2
 
 def test_character_can_level_up2():
-    Character1 = Character('Morgan', 'Chaotic-good-ish')
-    Character1.stats(15, 15, 15, 15, 15, 15)
+    Character1 = Character('Morgan', 'Chaotic-good-ish', strength=15)
+
 
     Character2 = Character('Keith', 'Lawful-Evil')
-    Character2.stats(17, 10, 15, 15, 15, 15)
+
 
     Character1.xp = 1990
 
-    Character1.attack(20, Character2)
+    Combat.attack(Character1, 20, Character2)
 
     assert Character1.xp == 2000
     assert Character1.player_level == 3
 
 def test_character_level_scales():
-    Character1 = Character('Morgan', 'Chaotic-good-ish')
-    Character1.stats(15, 15, 15, 15, 15, 15)
-    Character1.player_level = 2
+    Character1 = Character('Morgan', 'Chaotic-good-ish', constitution=15, player_level=2)
     assert Character1.max_HP == 14
-
-def test_defaults():
-    Character1 = Character('Morgan', 'CE', Character.DEFAULT_ATTRIBUTES)
 
 def test_new_dex():
     Character1 = Character('Dumb', 'align')
     Character1.dexterity = 20
     assert Character1.strength == 10
     assert Character1.dexterity == 20
+
+def test_character_death():
+    Character2 = Character('Keith', 'Lawful-Evil')
+    Character2.current_HP = 0
+    Character2.current_condition()
+    assert Character2.death == True
+
+def test_morgan_vs_keith():
+    Morgan = Character('Morgan', 'Chaotic-Good', strength=30)
+    Keith = Character('Keith', 'Lawful-Evil')
+
+    Combat.attack(Morgan, 20, Keith)
+
+    assert Keith.death == True
 
 
